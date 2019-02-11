bcc7449db87782023451841f02abee796216f1d0
James Chen <unknownguy2002@gmail.com>
2019-02-11 20:38:21 +0800

    Testing MakeCode Compatibility

--- serialiot.ts
+++ serialiot.ts
@@ -1,7 +1,7 @@
 /**
  * Custom blocks
  */
-//% color=#2b569b weight=10 icon="\uf1b9"
+//% color=#2b569b weight=10 icon="\uf287"
 
 namespace serialiot {
     /**
@@ -9,7 +9,7 @@ namespace serialiot {
     * @param data String to send
     */
     //% blockId=sendString weight=100
-    //% block="Sends info as a string to hub"
+    //% block="sendString"
     export function sendString(data: string): void {
         let strLength:number = data.length
         let packets: number = Math.ceil(strLength/18)
