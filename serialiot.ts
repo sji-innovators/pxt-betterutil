f3d4db74508bb9f53cfadbb6824ff18ac9c99216
James Chen <unknownguy2002@gmail.com>
2019-02-11 21:11:49 +0800

    Testing MakeCode Compatibility

--- serialiot.ts
+++ serialiot.ts
@@ -12,7 +12,14 @@ namespace serialiot {
     //% block="send String %data"
     export function sendString(data: string): void {
         let strLength:number = data.length
-        let packets: number = Math.ceil(strLength/18)
-        radio.sendString(`{"type: "header", data: {chars: ${strLength}, packets: ${packets} }')
+        let packets:number = Math.ceil(strLength/18)
+        radio.sendString(`{"type: "header", data: {chars: ${strLength}, packets: ${packets} }'`);
+
+        for (let i = 0; i < packets; i++) {
+            let sliced = data.slice(i*18, i+1*18)
+            console.log(sliced)
+        }
+        
+        return;
     }
 }
